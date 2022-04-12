77b3491212af6b4dbc5ce6a5e9f60976426127d1
Kevin Mao <hello@kmao.dev>
2022-04-12 12:47:06 +1000

    setup etherscan plugin + hardhat deployment, openzepplin installation

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,9 +13,29 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const RINKEBY_PRIVATE_KEY = "";
+
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/YzUO9BBO6O-DBXi27dTAD2fxdnsgfzpL',
+      accounts: [`${RINKEBY_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "RBTBZCNVW7I13Y7SHHA5WGRFMHKC6I8I6F"
+  }
 };
